3604cb3938a98cc2e40ab098f12b7aeba3687a5b
suarem298 <suarem298@gmail.com>
2019-10-02 22:48:42 -0400

    Add files via upload

--- database search.py
+++ database search.py
@@ -1,9 +1,10 @@
 import sqlite3
 from colormath.color_objects import LabColor
 from colormath.color_diff import delta_e_cie1976
-from tkinter import*
+from tkinter import *
 from functools import partial  
-
+from functools import reduce
+from tkinter import messagebox
 
 def create_connection(db_file):
     """ create a database connection to the SQLite database
@@ -21,19 +22,13 @@ def create_connection(db_file):
  
  
  
-def select_task_by_priority(a,b,c):
+def select_task_by_priority(conn,L,A,B):
     """
     Query tasks by priority
     :param conn: the Connection object
     :param priority:
     :return:
     """
-    conn = None
-    try:
-        conn = sqlite3.connect(db_file)
-    except:
-        print("test")
-
     
     cur = conn.cursor()
     cur.execute("SELECT Partnumber,Colorname,L,A,B FROM reading ")
@@ -43,8 +38,14 @@ def select_task_by_priority(a,b,c):
         color2=LabColor(lab_l=row[2], lab_a=row[3], lab_b=row[4])
         if(delta_e_cie1976(color1, color2)<3):
             print(row[0]," ",row[1])
-    
 
+def getcolor(conn,L,A,B):
+    messagebox.showinfo("Title", L+A+B)
+    #L=DoubleVar(L)
+    #A=DoubleVar(A)
+   # B=DoubleVar(B)
+    #select_task_by_priority(conn,L,A,B)
+    
 def main():
     database = r"C:\Users\icarus\python\ScanDatabase.db"
  
@@ -53,30 +54,31 @@ def main():
     root=Tk()
     root.geometry("250x250")
     
-    with conn:
-        L = Label(root, text = "Enter your L reading: ")
-        L.grid(row = 1, column = 0, padx = 0, pady = 10)
-        e1 = Entry(root)
-        e1.grid(row = 1, column = 1, padx = 0, pady = 10)
-        a=e1.get()
+    L = Label(root, text = "Enter your L reading: ")
+    L.grid(row = 1, column = 0, padx = 0, pady = 10)
+    light = Entry(root)
+    light.grid(row = 1, column = 1, padx = 0, pady = 10)
+    L=45
+        
 
         
-        A = Label(root, text ="Enter your A reading: ")
-        A.grid(row = 2, column = 0, padx = 0, pady = 10)
-        e2 = Entry(root)
-        e2.grid(row = 2, column = 1, padx = 0, pady = 10)
-        b=e2.get()
+    A = Label(root, text ="Enter your A reading: ")
+    A.grid(row = 2, column = 0, padx = 0, pady = 10)
+    red = Entry(root)
+    red.grid(row = 2, column = 1, padx = 0, pady = 10)
+    A=red.get()
+      
     
-        B = Label(root, text = "Enter your B reading: ")
-        B.grid(row = 3, column = 0, padx = 0, pady = 10)
-        e3 = Entry(root)
-        e3.grid(row = 3, column = 1, padx = 0, pady = 10)
-        c=e3.get()
-
+    B = Label(root, text = "Enter your B reading: ")
+    B.grid(row = 3, column = 0, padx = 0, pady = 10)
+    blue = Entry(root)
+    blue.grid(row = 3, column = 1, padx = 0, pady = 10)
+    B=blue.get()
+       
         
-        select_task_by_priority=partial(conn,a,b,c)
-        sbmitbtn = Button(root, text = "Submit",activebackground = "pink", activeforeground = "blue",command=lambda color1:self.select_task_by_priority(a,b,c))
-        sbmitbtn.grid(row = 4, column = 0, padx = 0, pady = 10)
+        #select_task_by_priority=partial(conn,L,A,B)
+    sbmitbtn = Button(root, text = "Submit",command=lambda: getcolor(conn,L,A,B))
+    sbmitbtn.grid(row = 4, column = 0, padx = 0, pady = 10)
        
  
     root.mainloop() 
